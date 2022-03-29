917233521f3dde85ec9c0018d624bc32e41a99ff
Amyr Francisco <amyrfrancisco11@gmail.com>
2022-03-29 12:32:12 +0800

    imported side bar on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:taxoplay/constants.dart';
-import 'package:taxoplay/models/screen.dart';
+
+import '../components/sidebar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,74 +12,6 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Home"),
       ),
       drawer: const SideBar(),
-    );
-  }
-}
-
-class SideBar extends StatelessWidget {
-  const SideBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const SafeArea(
-            child: SizedBox(
-              height: 65,
-              child: DrawerHeader(
-                child: Text(
-                  'Menu',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-          ),
-          buildListTile(context),
-          ListTile(
-            title: const Text('Stats'),
-            onTap: () {
-              //...
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Game Info'),
-            onTap: () {
-              //...
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Game Instruction'),
-            onTap: () {
-              //...
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Developers'),
-            onTap: () {
-              //...
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  ListTile buildListTile(BuildContext context) {
-    return ListTile(
-      selectedColor: kSecondaryColor,
-      selectedTileColor: Colors.white.withAlpha(20),
-      selected: true,
-      title: const Text('Home'),
-      onTap: () {
-        //...
-        Navigator.pop(context);
-      },
     );
   }
 }
