09a78a280d75fb09e9ab8dff589f2ff75f085b20
Amyr Francisco <amyrfrancisco11@gmail.com>
2022-04-03 20:44:52 +0800

    extracted widget for prize and question

--- lib/screens/puzzle/puzzle_screen.dart
+++ lib/screens/puzzle/puzzle_screen.dart
@@ -83,28 +83,7 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      padding: EdgeInsets.only(top: kDefaultSpace / 2),
-                      child: Text(
-                        '\$${widget.question.price}',
-                        style: const TextStyle(fontSize: 34),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: kDefaultSpace / 3,
-                        vertical: kDefaultSpace / 2,
-                      ),
-                      child: Text(
-                        widget.question.question,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: kSecondaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
+                    PrizeAndQuestion(question: widget.question),
                     Container(
                       padding: const EdgeInsets.symmetric(horizontal: 10),
                       child: LayoutBuilder(
@@ -222,6 +201,45 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class PrizeAndQuestion extends StatelessWidget {
+  const PrizeAndQuestion({
+    Key? key,
+    required this.question,
+  }) : super(key: key);
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: kDefaultSpace / 2),
+          child: Text(
+            '\$${question.price}',
+            style: const TextStyle(fontSize: 34),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: kDefaultSpace / 3,
+            vertical: kDefaultSpace / 2,
+          ),
+          child: Text(
+            question.question,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: kSecondaryColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
