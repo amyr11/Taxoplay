8550e1521167bcc3948bceefffdfbbc061f15c82
Amyr Francisco <amyrfrancisco11@gmail.com>
2022-04-08 14:24:22 +0800

    added format_question file

---
+++ lib/helpers/format_questions.dart
@@ -0,0 +1,28 @@
+import 'package:taxoplay/models/category.dart';
+
+List<Question> easy = [
+  PuzzleQuestion(
+    100,
+    'Greek Philosopher who saw the hierarchy of organisms called the “Ladder of Nature"',
+    'ARISTOTLE',
+    [2, 4, 8],
+  ),
+  PuzzleQuestion(
+    100,
+    'One of the three major domains',
+    'EUKARYA',
+    [0, 2, 6],
+  ),
+  PuzzleQuestion(
+    200,
+    'Animals with bony backbones',
+    'VERTEBRATES',
+    [0, 2, 6, 10],
+  ),
+  PuzzleQuestion(
+    200,
+    'Smallest category in the hierarchical classification of organisms',
+    'SPECIES',
+    [0, 1, 6],
+  ),
+];
